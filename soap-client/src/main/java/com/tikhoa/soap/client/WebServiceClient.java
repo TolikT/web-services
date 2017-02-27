2e12100f1574240114807e9133158d356bbb9793
TolikT <anatoliytihomirov@yahoo.com>
2017-02-27 12:42:34 -0800

    standalone part completed - client updated with example

--- soap-client/src/main/java/com/tikhoa/soap/client/WebServiceClient.java
+++ soap-client/src/main/java/com/tikhoa/soap/client/WebServiceClient.java
@@ -14,12 +14,23 @@ public class WebServiceClient {
     public static void main(String[] args) throws MalformedURLException {
         URL url = new URL("http://localhost:8080/PersonService?wsdl");
         PersonService personService = new PersonService(url);
+        Person search = new Person();
+        search.setName("Ольга");
+        search.setSurname("Бергольц");
         List<Person> persons = personService.getPersonWebServicePort().getPersons();
+        List<Person> searchPersons = personService.getPersonWebServicePort().getPersonsByParameters(search);
         for (Person person : persons) {
             System.out.println("name: " + person.getName()
                     + ", surname: " + person.getSurname() + ", age: " + person.getAge()
                     + ", isEmployee: " + String.valueOf(person.getIsEmployee()) + ", contactDate: " + person.getContactDate());
         }
         System.out.println("Total persons: " + persons.size());
+        for (Person person : searchPersons) {
+            System.out.println("name: " + person.getName()
+                    + ", surname: " + person.getSurname() + ", age: " + person.getAge()
+                    + ", isEmployee: " + String.valueOf(person.getIsEmployee()) + ", contactDate: " + person.getContactDate());
+        }
+        System.out.println("Total found persons: " + searchPersons.size());
+        
     }
 }
