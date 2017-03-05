0b795d7f926687614e71ac3d3b3084c68a22dd2a
TolikT <anatoliytihomirov@yahoo.com>
2017-03-05 03:05:55 -0800

    used java reflection to parse fields

--- soap-standalone/src/main/java/com/tikhoa/soap/standalone/PersonWebService.java
+++ soap-standalone/src/main/java/com/tikhoa/soap/standalone/PersonWebService.java
@@ -5,8 +5,11 @@
  */
 package com.tikhoa.soap.standalone;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
@@ -19,30 +22,59 @@ public class PersonWebService {
         List<Person> persons = dao.getPersons("");
         return persons;
     }
-    
+
     @WebMethod(operationName = "getPersonsByParameters")
     public List<Person> getPersonsByParameters(Person params) {
+
         String condition = "where ";
-        if (params.getAge() != null) {
+
+        Field[] fields = params.getClass().getDeclaredFields();
+        for (Field f : fields) {
+            try {
+                f.setAccessible(true);
+                Class t = f.getType();
+                Object v = f.get(params);
+                String n = f.getName();
+                if (!t.isPrimitive() && v != null && !v.toString().equals("")) // found not default value
+                {
+                    condition += String.format("%s=\'%s\' and ", n, v);
+                }
+            } catch (IllegalArgumentException ex) {
+                Logger.getLogger(PersonWebService.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                Logger.getLogger(PersonWebService.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        /*if (params.getAge () 
+        != null) {
             condition += "age=\'" + params.getAge().toString() + "\' and ";
-        }
-        if (params.getName() != null) {
+    }
+
+    if (params.getName () 
+        != null) {
             condition += "name=\'" + params.getName().toString() + "\' and ";
-        }
-        if (params.getSurname()!= null) {
+    }
+
+    if (params.getSurname () 
+        != null) {
             condition += "surname=\'" + params.getSurname().toString() + "\' and ";
-        }
-        if (params.getIsEmployee() != null) {
+    }
+
+    if (params.getIsEmployee () 
+        != null) {
             condition += "is_employee=\'" + params.getIsEmployee().toString().toLowerCase() + "\' and ";
-        }
-        if (params.getContactDate() != null) {
+    }
+
+    if (params.getContactDate () 
+        != null) {
             condition += "contact_date=\'" + params.getContactDate().toString() + "\' and ";
-        }
+    }*/
         condition = condition.substring(0, condition.length() - 4);
+
         System.out.println(condition);
         PostgreSQLDAO dao = new PostgreSQLDAO();
         List<Person> persons = dao.getPersons(condition);
         return persons;
     }
-
 }
